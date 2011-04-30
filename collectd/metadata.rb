43494a20b8caa198bd63023fab1e74294e1e1951
Anthony Goddard <goddardanthony@gmail.com>
2011-04-29 21:50:35 -0400

    added debian support in metadata

--- collectd/metadata.rb
+++ collectd/metadata.rb
@@ -4,4 +4,7 @@ license          "Apache 2.0"
 description      "Install and configure the collectd monitoring daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0.0"
-supports         "ubuntu"
+
+%w{debian ubuntu}.each do |os|
+  supports os
+end
